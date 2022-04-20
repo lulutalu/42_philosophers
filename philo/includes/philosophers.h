214798a1a6f7fe2b7dab8286e77820507605ab33
Loic Dubouloz <lduboulo@c1r13s5.42lausanne.ch>
2022-04-20 21:03:43 +0200

    Start of the project

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduboulo <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 20:43:18 by lduboulo          #+#    #+#             */
+/*   Updated: 2022/04/20 21:03:23 by lduboulo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+/*
+ * Default Library
+*/
+
+# include "philosophers.h"
+
+/*
+ * Authorized Functions Library
+*/
+
+# include <string.h>	/* memset function */
+# include <stdio.h> 	/* printf function */
+# include <stdlib.h>	/* malloc, free functions */
+# include <unistd.h>	/* write, usleep functions */
+# include <sys/time.h>	/* gettimeofday function */
+# include <pthread.h>	/* threads and mutex functions */
+
+#endif
